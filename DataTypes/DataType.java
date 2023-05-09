b1b41900c6402c6f649533bdd9e975eac017bba3
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-05-09 17:27:30 +0400

    java

---
+++ DataTypes/DataType.java
@@ -0,0 +1,22 @@
+class DataType {
+    public static void main(String[] args) {
+        //integer types
+        byte aSingleByte = 100; // -128 to 127
+        short aSmallNumber = 20000; // -32,768 to 32.767
+        int anInteger = 2147483647; // -2147483648 to 2147483647
+        long aLargeNumber = 9223372036854775807L; // 9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        //decimal types
+        double aDouble = 1.79769313; // Sufficient for storing 15 decimal digits
+        float aFloat = 3.4028F; // 1.4E-45 to 3.4028235E38
+
+        // booleans
+        boolean isLazo = false;
+        boolean isNotLazo = true;
+
+        //characters
+        char symbol = '\u00A9'; // Stores a single character/letter or ASCII values
+
+
+    }
+}
---
+++ old.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] argsuments) {
+    int age = 20;
+    age = 1;
+    System.out.println("I am " + age + "years old");
+  }
+}
