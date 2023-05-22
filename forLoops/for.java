9d73a55961ccf3b6accfe7c194bce11599361d1b
itachi2003 <lomsaadze.lazare2003@gmail.com>
2023-05-23 01:07:34 +0400

    add forloop

---
+++ forLoops/for.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+      int number = 5;
+      for(int another = 1; another < 10; another++) {
+        System.out.printf("%d X %d = %d \n", number, another, number * another);
+      }
+    }
+  }
---
+++ forLoops/forI-n-for.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+      for(int another = 1; another <= 10; another++) {
+        for(int gym = 1; gym <= 10; gym++) {
+        System.out.printf("%d X %d = %d \n", another, gym, another * gym);
+      }
+      }
+    }
+  }
